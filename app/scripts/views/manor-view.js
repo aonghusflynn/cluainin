a98792cb7f7da6cc3465d28754084d4c9cf66bbb
Aonghus Flynn <developer@aonghusflynn.com>
2012-11-15 23:12:04 +0000

    changed the images to a smaller size.

--- app/scripts/views/manor-view.js
+++ app/scripts/views/manor-view.js
@@ -1,18 +1,23 @@
 cluainin.Views.manorView = Backbone.View.extend({
 
   //template: manor
+  contentSet:false,
   el: "#about",
-  initialize: function(){
+  initialize: function(aboutSet){
+    var self = this;
+    if(!aboutSet){
+      $(this.el).append("<h2>Why visit?</h2>");
+      $(this.el).append("<p>Sports, arts, fishing </p>");
+      $(this.el).append("<p>Whether it's sports, arts or just a quiet day fishing.</p>");
+      $(".about").css('height', window.innerHeight+'px');
+      aboutSet = true;
+    }
   	this.render();
   },
 
   render: function(){
-    
+    $('#slider').animate({top: -60});
     window.onresize=function(){$("#viewport").css('height', window.innerHeight-60+'px');};
-    var self = this;
-    $(this.el).append("<h2>Why visit?</h2>");
-    $(this.el).append("<p>Sports, arts, fishing </p>");
-    $(this.el).append("<p>Whether it's sports, arts or just a quiet day fishing.</p>");
-    $(".about").css('height', window.innerHeight+'px');
+    return this;
   }
 });
--- app/scripts/views/sport-view.js
+++ app/scripts/views/sport-view.js
@@ -2,15 +2,17 @@ cluainin.Views.sportView = Backbone.View.extend({
 	el: '#sport',
   //template: sport
 	initialize: function(){
+    var self = this;
+      $(this.el).append("<h2>Sport</h2>");
+      $(this.el).append("<p>Sports, arts, fishing </p>");
+      $(this.el).append("<p>Fishing etc</p>");
+      $(".sport").css('height', window.innerHeight+'px');
   		this.render();
   	},
 
   	render: function(){
+      $('#slider').animate({top: -(window.innerHeight+60)})
     	window.onresize=function(){$("#viewport").css('height', window.innerHeight-60+'px');};
-    	var self = this;
-	    $(this.el).append("<h2>Sport</h2>");
-	    $(this.el).append("<p>Sports, arts, fishing </p>");
-	    $(this.el).append("<p>Fishing etc</p>");
-	    $(".sport").css('height', window.innerHeight+'px');
+    	
   	}
 });
