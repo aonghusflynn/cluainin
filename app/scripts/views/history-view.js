f2d7ffe7656cf88a015eca44e72dc5b6f62e0324
Aonghus Flynn <aflynn@aflynn-ltm2.internal.salesforce.com>
2012-11-21 16:13:37 +0000

    added google analytics

--- app/scripts/views/history-view.js
+++ app/scripts/views/history-view.js
@@ -7,8 +7,9 @@ cluainin.Views.historyView = Backbone.View.extend({
       var self = this;
       $(this.el).empty();
       $(this.el).append("<h2>History</h2>");
-      $(this.el).append("<p>Sports, arts, fishing </p>");
-      $(this.el).append("<p>Fishing etc</p>");
+      $(this.el).append("<p>Steeped in Irish history, even the name of the town is lesson in Ireland's past. Originally called Cluainin Ui Ruairc, or the little meadow of O'Rourke (the local chieftan), the area was taken from the Irish and given to Frederick Hamilton in 1622.</p>");
+      $(this.el).append("<p>Here, he built his <a href='http://www.manorhamiltoncastle.ie/'>castle</a> or manor house, from where he ruled the area.</p>");
+      $(this.el).append("<p>This is where the name Manorhamilton came from, but even today, the town is called Cluainin in the Irish language.</p>");
       $(".section").css('height', window.innerHeight+'px');
   		this.render();
   	},
--- app/scripts/views/sport-view.js
+++ app/scripts/views/sport-view.js
@@ -5,8 +5,9 @@ cluainin.Views.sportView = Backbone.View.extend({
       var self = this;
       $(this.el).empty();
       $(this.el).append("<h2>Sport</h2>");
-      $(this.el).append("<p>Sports, arts, fishing </p>");
-      $(this.el).append("<p>Fishing etc</p>");
+      $(this.el).append("<p>Situated in the North West of Ireland, Manorhamilton is an ideal location for your spotring holiday.</p>");
+      $(this.el).append("<p>Whether it's fishing for the <a href='http://en.wikipedia.org/wiki/Gillaroo'>Gillaroo</a> in Lough Melvin, a round of golf in <a href='http://www.donegalcottageholidays.com/blog/ballyliffin/north-west-irelandeuropean-golf-destination-of-the-year-2011/1337/'>2011's best golfing location in Europe</a>, Manorhamilton is where you should be based.</p>");
+      $(this.el).append("<p>Looing for something a bit more active? Manorhamilton is a short drive from some of the best surfing on the west coast of Ireland.</p>");
       $(".section").css('height', window.innerHeight+'px');
       $('#slider').css('top',-(window.innerHeight+60));
   		this.render();
